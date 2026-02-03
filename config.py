1bfe68ccec80a95a7e7f907e0fef73e040eed1b0
UCHIHABOTS <txkuze@gmail.com>
2026-02-03 23:41:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/itzarjuna1/Dark-bot-infinityX",
+    "https://github.com/txkuze/snowy",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
