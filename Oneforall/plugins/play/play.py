d6afd8fc3c362d3fac30388fff6ddaa4a03d8f9f
UCHIHABOTS <txkuze@gmail.com>
2026-02-19 18:58:20 +0530

    Update play.py

--- Oneforall/plugins/play/play.py
+++ Oneforall/plugins/play/play.py
@@ -394,7 +394,7 @@ async def play_commnd(
             )
             await mystic.delete()
             await message.reply_photo(
-                photo= config.YOUTUBE_IMG_URL
+                photo= config.YOUTUBE_IMG_URL,
                 has_spoiler=True,
                 caption=cap,
                 reply_markup=InlineKeyboardMarkup(buttons),
@@ -413,7 +413,7 @@ async def play_commnd(
                 )
                 await mystic.delete()
                 await message.reply_photo(
-                    photo= config.YOUTUBE_IMG_URL
+                    photo= config.YOUTUBE_IMG_URL,
                     caption=_["play_10"].format(
                         details["title"].title(),
                         details["duration_min"],
@@ -432,7 +432,7 @@ async def play_commnd(
                 )
                 await mystic.delete()
                 await message.reply_photo(
-                    photo=img,
+                    photo= config.YOUTUBE_IMG_URL,
                     has_spoiler=True,
                     caption=cap,
                     reply_markup=InlineKeyboardMarkup(buttons),
