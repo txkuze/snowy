83dfabe47c8727ad760b051b8f5d95fb7ec54874
UCHIHABOTS <txkuze@gmail.com>
2026-02-19 17:53:12 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], callback_data="OWNER_ID"),
+            InlineKeyboardButton(text=_["S_B_5"], url=config.OWNER_ID),
         ],
         [
             InlineKeyboardButton(text="✦ ᴡєʙ ɢᴧϻєꜱ 🎮✨", url="https://telegram-game-hub.vercel.app"),
